1492fc4c729680940376702f8f3db1450c15836d
Mirsilent <75432338+Mirsilent@users.noreply.github.com>
2024-12-19 13:17:40 +0500

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+pip3 install -r requirements.txt  # Use pip3 instead of pip
+python3 manage.py collectstatic --noinput --clear  # Use python3 instead of python
 echo "BUILD END"
